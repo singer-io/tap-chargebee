21988d93fe1d7de7ef4619a6071322134ab9ae9c
dbshah1212 <root@ubuntu16.cdsys.local>
2021-06-09 18:04:44 +0530

    TDL-6624: Added comments stream

---
+++ tap_chargebee/streams/comments.py
@@ -0,0 +1,16 @@
+from tap_chargebee.streams.base import BaseChargebeeStream
+
+class CommentsStream(BaseChargebeeStream):
+    TABLE = 'comments'
+    ENTITY = 'comment'
+    REPLICATION_METHOD = 'INCREMENTAL'
+    REPLICATION_KEY = 'created_at'
+    KEY_PROPERTIES = ['id']
+    BOOKMARK_PROPERTIES = ['created_at']
+    SELECTED_BY_DEFAULT = True
+    VALID_REPLICATION_KEYS = ['created_at']
+    INCLUSION = 'available'
+    API_METHOD = 'GET'
+
+    def get_url(self):
+        return 'https://{}.chargebee.com/api/v2/comments'.format(self.config.get('site'))
