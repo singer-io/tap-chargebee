ac78b9af4a72f809b935f4841d4656c1ac75adfc
cb-prasanna <sairam@chargebee.com>
2021-06-24 18:43:10 +0530

    GEN-763 Added Quotes Object for Syncing to Stitch

--- tap_chargebee/streams/__init__.py
+++ tap_chargebee/streams/__init__.py
@@ -15,6 +15,7 @@ from .virtual_bank_accounts import VirtualBankAccountsStream
 from .credit_notes import CreditNotesStream
 from .gifts import GiftsStream
 from .orders import OrdersStream
+from .quotes import QuotesStream
 from .promotional_credits import PromotionalCreditsStream
 
 COMMON_AVAILABLE_STREAMS = [
@@ -26,6 +27,7 @@ COMMON_AVAILABLE_STREAMS = [
     InvoicesStream,
     OrdersStream,
     PaymentSourcesStream,
+    QuotesStream
     PromotionalCreditsStream,
     SubscriptionsStream,
     TransactionsStream,
