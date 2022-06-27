e4575ba79a33bb7513cff79f0da6cf693718713d
savan-chovatiya <80703490+savan-chovatiya@users.noreply.github.com>
2022-06-27 11:01:36 +0530

    TDL-19101: Add custom exception handling (#85)
    * Added custom exception handling
    
    * Added unit tests
    
    * Updated config.yml to cover unit test report
    
    * Added condition to handle status code other than 4xx 5xx
    
    * Added condition to handle status code other than 4xx 5xx
    
    * Added/updated unit tests
    
    * Added existing unit tests
    
    * Resolved review comment
    
    * Fixed unit test
    
    * Resolved review comment
    
    * Removed redundent Server429Error
    
    Co-authored-by: harshpatel4crest <harsh.patel4@crestdatasys.com>

--- tap_chargebee/client.py
+++ tap_chargebee/client.py
@@ -15,13 +15,119 @@ LOGGER = singer.get_logger()
 # timeout request after 300 seconds
 REQUEST_TIMEOUT = 300
 
-class Server4xxError(Exception):
+class ChargebeeError(Exception):
+    pass
+
+class Server4xxError(ChargebeeError):
+    pass
+
+class Server5xxError(ChargebeeError):
+    pass
+
+class ChargebeeBadRequestError(Server4xxError):
+    pass
+
+class ChargebeeAuthenticationError(Server4xxError):
+    pass
+
+class ChargebeeForbiddenError(Server4xxError):
+    pass
+
+class ChargebeeNotFoundError(Server4xxError):
+    pass
+
+class ChargebeeMethodNotAllowedError(Server4xxError):
+    pass
+
+class ChargebeeNotProcessedError(Server4xxError):
+    pass
+
+class ChargebeeRateLimitError(Server4xxError):
+    pass
+
+class ChargebeeInternalServiceError(Server5xxError):
+    pass
+
+class ChargebeeServiceUnavailableError(Server5xxError):
     pass
 
 
-class Server429Error(Exception):
-    pass
+STATUS_CODE_EXCEPTION_MAPPING = {
+    400: {
+        "raise_exception": ChargebeeBadRequestError,
+        "message": "The request URI does not match the APIs in the system.",
+    },
+    401: {
+        "raise_exception": ChargebeeAuthenticationError,
+        "message": "The user is not authenticated to use the API.",
+    },
+    403: {
+        "raise_exception": ChargebeeForbiddenError,
+        "message": "The requested operation is not permitted for the user.",
+    },
+    404: {
+        "raise_exception": ChargebeeNotFoundError,
+        "message": "The requested resource was not found.",
+    },
+    405: {
+        "raise_exception": ChargebeeMethodNotAllowedError,
+        "message": "The HTTP action is not allowed for the requested REST API.",
+    },
+    409: {
+        "raise_exception": ChargebeeNotProcessedError,
+        "message": "The request could not be processed because of conflict in the request.",
+    },
+    429: {
+        "raise_exception": ChargebeeRateLimitError,
+        "message": "You are requesting to many requests.",
+    },
+    500: {
+        "raise_exception": ChargebeeInternalServiceError,
+        "message": "The request could not be processed due to internal server error.",
+    },
+    503: {
+        "raise_exception": ChargebeeServiceUnavailableError,
+        "message": "The request could not be processed due to temporary internal server error.",
+    },
+}
 
+
+def get_exception_for_status_code(status_code):
+    """Map the input status_code with the corresponding Exception Class \
+        using 'STATUS_CODE_EXCEPTION_MAPPING' dictionary."""
+
+    exception = STATUS_CODE_EXCEPTION_MAPPING.get(status_code, {}).get(
+                "raise_exception")
+    # If exception is not mapped for any code then use Server4xxError and Server5xxError respectively
+    if not exception:
+        if status_code > 400 and status_code < 500:
+            exception = Server4xxError
+        elif status_code > 500:
+            exception = Server5xxError
+        else:
+            exception = ChargebeeError
+    return exception
+
+def raise_for_error(response):
+    """Raises error class with appropriate msg for the response"""
+    try:
+        json_response = response.json()
+
+    except Exception:
+        json_response = {}
+
+    status_code = response.status_code
+
+    msg = json_response.get(
+        "message",
+        STATUS_CODE_EXCEPTION_MAPPING.get(status_code, {}).get(
+            "message", "Unknown Error"
+        ),
+    )
+    message = "HTTP-error-code: {}, Error: {}".format(status_code, msg)
+
+    exc = get_exception_for_status_code(status_code)
+    raise exc(message) from None
 
 class ChargebeeClient(BaseClient):
 
@@ -54,7 +160,7 @@ class ChargebeeClient(BaseClient):
         return params
 
     @backoff.on_exception(backoff.expo,
-                          (Server4xxError, Server429Error, Timeout, ConnectionError),
+                         (Server4xxError, Server5xxError, Timeout, ConnectionError),
                           max_tries=5,
                           factor=3)
     @utils.ratelimit(100, 60)
@@ -81,19 +187,7 @@ class ChargebeeClient(BaseClient):
             json=body,
             timeout=request_timeout)
 
-        try:
-            response_json = response.json()
-        except simplejson.scanner.JSONDecodeError:
-            # Formatted error message for json decoder error
-            response_json = {
-                "message": "Did not get response from the server due to an unknown error.",
-                "http_status_code": response.status_code
-            }
+        if response.status_code != 200:
+            raise_for_error(response)
 
-        if response.status_code == 429:
-            raise Server429Error()
-
-        if response.status_code >= 400:
-            raise Server4xxError(response_json)
-
-        return response_json
+        return response.json()
