ab309bc39358fddc19b1585ef70b9b3153aa63ae
prijendev <prijen.khokhani@qlik.com>
2026-04-16 14:16:24 +0530

    Update unit tests

--- tap_chargebee/streams/base.py
+++ tap_chargebee/streams/base.py
@@ -169,35 +169,6 @@ class BaseChargebeeStream:
             }
         ]
 
-    def appendCustomFields(self, record: dict):
-        """
-        Prepare custom fields for the record for objects like "addon", "plan", "subscription", "customer" from the /events endpoint
-        """
-        listOfCustomFieldObj = ["addon", "plan", "subscription", "customer"]
-        custom_fields = {}
-        event_custom_fields = {}
-
-        if self.ENTITY == "event":
-            # Extracting the object name from the event_type and adding custom fields for the object
-            words = record["event_type"].split("_")
-            content_obj = "_".join(words[:-1])
-            content_data = record["content"].get(content_obj, {})
-
-            # Add custom fields for specific objects
-            if content_obj in listOfCustomFieldObj:
-                for k, v in content_data.items():
-                    if "cf_" in k:
-                        event_custom_fields[k] = v
-                record["content"][content_obj]["custom_fields"] = json.dumps(
-                    event_custom_fields
-                )
-
-        for k, v in record.items():
-            if "cf_" in k:
-                custom_fields[k] = v
-        record["custom_fields"] = json.dumps(custom_fields)
-        return record
-
     def update_bookmark(self, bookmark_value: str):
         """
         Updates the bookmark in the state if the new bookmark value is greater than the current one.
