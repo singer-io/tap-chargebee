bc8dd10fef943f64905ca591c2471fe51c2c2210
Tushar Mittal <tushar.mittal@qlik.com>
2026-07-24 14:31:30 +0530

    [SAC-30996] - Unauthorized streams exclusion from catalog during discovery (#125)
    * unauth streams exclusion
    
    * copilot review addressed
    
    * libraries upgrades
    
    * accessible streams name correction
    
    * stream name referenced directly

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-chargebee',
-      version='1.7.1',
+      version='1.8.0',
       description='Singer.io tap for extracting data from the Chargebee API',
       author='dwallace@envoy.com',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_chargebee'],
       install_requires=[
-          'singer-python==6.3.0',
+          'singer-python==6.8.0',
           'backoff==2.2.1',
-          'requests==2.33.0'
+          'requests==2.34.2'
       ],
       entry_points='''
           [console_scripts]
